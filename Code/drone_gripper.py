0b3112e5ca2e9e422c0262bbad336ce50bc96658
jjurgello <jjurgello@gmail.com>
2021-05-11 18:20:51 -0500

    add model links and base code

--- Code/drone_gripper.py
+++ Code/drone_gripper.py
@@ -2,7 +2,7 @@ from gripper import GripperInfo
 import os
 data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'../drone_data'))
 
-airobot_grip = GripperInfo("airobot_grip",0,[1,2,3,4,5,6,7,8],[0],
+airobot_grip = GripperInfo("airobot_grip",0,[0],[0],
     klampt_model=os.path.join(data_dir,"/airobot.urdf"))
 airobot_grip.center = (0,0,0.1)
 airobot_grip.primary_axis = (0,0,1)
